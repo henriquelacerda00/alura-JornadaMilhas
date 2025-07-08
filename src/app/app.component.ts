b12377c431988ee8cf7d2cdfd0ea2f9be732cc40
Henrique Lacerda <henriquelacerda69@gmail.com>
2025-07-08 17:03:29 -0300

    alert on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    alert('Back-end hospedado na fly.io com plano gratuito, aguarde 2 - 5 segundos para inicializar o serviço.');
+  }
   title = 'jornada-milhas';
 }
